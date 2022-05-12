8e651d3c9a1770bd00371a7d95d66bcc831c1bc7
LSX99 <shengxianglim99@gmail.com>
2022-05-12 23:34:36 +0800

    updates from sx

--- app/src/main/java/com/example/nftscmers/applicantactivities/EditProfileActivity.java
+++ app/src/main/java/com/example/nftscmers/applicantactivities/EditProfileActivity.java
@@ -1,13 +1,87 @@
 package com.example.nftscmers.applicantactivities;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nftscmers.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditProfileActivity extends AppCompatActivity {
+
+    private static final String TAG = "EditProfileActivity";
+
+    ImageView IV_profile_pic;
+    EditText ET_name;
+    EditText ET_about;
+    TextView TV_skills;
+    ListView LV_skills;
+    Button BT_confirm;
+
+    List<String> skills_list = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_applicant_edit_profile);
+
+        IV_profile_pic = findViewById(R.id.iv_profile_pic);
+        ET_name = findViewById(R.id.et_name);
+        ET_about = findViewById(R.id.et_about);
+        TV_skills = findViewById(R.id.tv_skills);
+        LV_skills = findViewById(R.id.lv_skills);
+        BT_confirm = findViewById(R.id.button_confirm);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,skills_list);
+        LV_skills.setAdapter(arrayAdapter);
+
+        TV_skills.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
     }
+
+
+    /**
+     * This function shows the dialog box
+     */
+    void showCustomDialog() {
+        final Dialog dialog = new Dialog(EditProfileActivity.this);
+        //Disable default title so that the custom title "Skills" will show.
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //The user will be able to cancel the dialog by clicking anywhere outside the dialog.
+        dialog.setCancelable(true);
+        //Mention the name of the layout of your custom dialog.
+        dialog.setContentView(R.layout.dialog_edit_profile_skills);
+
+        // Initializing the views of the dialog.
+        EditText ET_skill_name = dialog.findViewById(R.id.et_skill_name);
+        Button BT_add = dialog.findViewById(R.id.button_add);
+
+        // TODO: update firestore and show on listview
+        BT_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String skill_name = ET_skill_name.getText().toString();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
 }
--- app/src/main/java/com/example/nftscmers/employeractivities/CreateJobActivity.java
+++ app/src/main/java/com/example/nftscmers/employeractivities/CreateJobActivity.java
@@ -1,13 +1,122 @@
 package com.example.nftscmers.employeractivities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nftscmers.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ktx.Firebase;
+
 public class CreateJobActivity extends AppCompatActivity {
+    private static final String TAG = "CreateJobActivity";
+    FirebaseFirestore db;
+
+    // UI Elements
+    ImageView back_button;
+    TextView create_job;
+    TextView employer_name;
+    EditText edit_job;
+    EditText edit_job_description;
+    EditText edit_job_deadline;
+    EditText edit_job_location;
+    TextView edit_skills;
+    TextView edit_tags;
+
+    Button confirm_button;
+
+
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer_create_job);
+
+        // initialise Firestore db
+        db = FirebaseFirestore.getInstance();
+
+        // initialise UI elements
+        back_button = findViewById(R.id.back_button);
+        create_job = findViewById(R.id.create_job);
+        employer_name = findViewById(R.id.employer_name);
+        edit_job = findViewById(R.id.edit_job);
+        edit_job_description = findViewById(R.id.edit_job_description);
+        edit_job_deadline = findViewById(R.id.edit_job_deadline);
+        edit_job_location = findViewById(R.id.edit_job_location);
+        edit_skills = findViewById(R.id.edit_skills);
+        edit_tags = findViewById(R.id.edit_tags);
+        confirm_button = findViewById(R.id.confirm_button);
+
+        // initialise buttons
+        //TODO: Back Button -> Go back to previous page
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreateJobActivity.this, ViewApplicationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        edit_job.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        edit_job_description.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        edit_job_deadline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        edit_job_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        edit_skills.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        edit_tags.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+             }
+         });
+
+        confirm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
     }
-}
+    }
+
