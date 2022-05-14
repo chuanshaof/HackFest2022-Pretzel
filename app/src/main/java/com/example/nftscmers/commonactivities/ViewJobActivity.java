eb2c31d55b874bb97d337b5f287e61ece017a663
munnigel <munnigel@yahoo.com.sg>
2022-05-14 17:35:38 +0800

    Added swiping feature for applicant and employer

--- app/src/main/java/com/example/nftscmers/commonactivities/ViewJobActivity.java
+++ app/src/main/java/com/example/nftscmers/commonactivities/ViewJobActivity.java
@@ -1,13 +1,127 @@
 package com.example.nftscmers.commonactivities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nftscmers.R;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewJobActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> arrayAdapter;
+    List<String> data;
+    SwipeFlingAdapterView flingAdapterView;
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    DatabaseReference reference;
+    public static final String TAG = "YOUR-TAG-NAME";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.like_dislike_button_bottom);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        flingAdapterView=findViewById(R.id.swipe);
+        data=new ArrayList<>();
+        data.add("1");
+
+        db.collection("Employers")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                        Log.d(TAG, document.getId() + " => " + document.getData());
+                                data.add(document.getId() + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+        arrayAdapter=new ArrayAdapter<>(ViewJobActivity.this, R.layout.item_in_cardview, R.id.data, data);
+
+        flingAdapterView.setAdapter(arrayAdapter);
+
+        flingAdapterView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                data.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object o) {
+
+                Toast.makeText(ViewJobActivity.this,"dislike",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object o) {
+
+                Toast.makeText(ViewJobActivity.this,"like",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int i) {
+
+            }
+
+            @Override
+            public void onScroll(float v) {
+
+            }
+        });
+
+        flingAdapterView.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int i, Object o) {
+                Toast.makeText(ViewJobActivity.this, "data is "+data.get(i),Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button like,dislike;
+
+        like=findViewById(R.id.like);
+        dislike=findViewById(R.id.dislike);
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flingAdapterView.getTopCardListener().selectRight();
+            }
+        });
+        dislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flingAdapterView.getTopCardListener().selectLeft();
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/nftscmers/employeractivities/ViewApplicationActivity.java
+++ app/src/main/java/com/example/nftscmers/employeractivities/ViewApplicationActivity.java
@@ -1,13 +1,131 @@
 package com.example.nftscmers.employeractivities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nftscmers.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+
 public class ViewApplicationActivity extends AppCompatActivity {
+
+    private ArrayAdapter<String> arrayAdapter;
+
+    SwipeFlingAdapterView flingAdapterView;
+    public static final String TAG = "YOUR-TAG-NAME";
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.like_dislike_button_bottom);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        flingAdapterView=findViewById(R.id.swipe);
+
+        ArrayList data=new ArrayList<>();
+//        data.add("Welcome");
+
+
+        //Iterating through list of applicant details in firebase and displaying on card swipe
+        db.collection("Accounts")
+                .get()
+        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                        Log.d(TAG, document.getId() + " => " + document.getData());
+                        data.add(document.getId() + document.getData());
+                    }
+                    arrayAdapter.notifyDataSetChanged();
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
+                }
+            }
+        });
+
+
+
+
+
+
+
+        arrayAdapter=new ArrayAdapter<>(ViewApplicationActivity.this, R.layout.item_in_cardview, R.id.data, data);
+
+        flingAdapterView.setAdapter(arrayAdapter);
+
+        flingAdapterView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                data.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object o) {
+
+                Toast.makeText(ViewApplicationActivity.this,"dislike",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object o) {
+
+                Toast.makeText(ViewApplicationActivity.this,"like",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int i) {
+
+            }
+
+            @Override
+            public void onScroll(float v) {
+
+            }
+        });
+
+        flingAdapterView.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int i, Object o) {
+                Toast.makeText(ViewApplicationActivity.this, "data is "+data.get(i),Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button like,dislike;
+
+        like=findViewById(R.id.like);
+        dislike=findViewById(R.id.dislike);
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flingAdapterView.getTopCardListener().selectRight();
+            }
+        });
+        dislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flingAdapterView.getTopCardListener().selectLeft();
+            }
+        });
+
     }
-}
+
+
+};
+
+
